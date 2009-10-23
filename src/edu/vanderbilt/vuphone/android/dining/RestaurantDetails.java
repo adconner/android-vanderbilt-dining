06a796439122150e324932422db0913b7ead9f91
aconner.vu <aconner.vu@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-10-23 02:42:50 +0000

    Formatted details page a bit more. Fixed button (the interface is going to need an overhaul sometime anyway...)

--- src/edu/vanderbilt/vuphone/android/dining/RestaurantDetails.java
+++ src/edu/vanderbilt/vuphone/android/dining/RestaurantDetails.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.style.RelativeSizeSpan;
 import android.util.Log;
@@ -36,26 +37,29 @@ public class RestaurantDetails extends Activity {
 		
 		rightNow = Calendar.getInstance();
 		restaurant = getIntent().getExtras().getInt("restaurant");
-
-		((TextView) findViewById(R.restaurantDetailsPage.restaurantName)).setText(Main.RESTAURANTS[restaurant]); 
+		
+		
+		SpannableString title = new SpannableString(Main.RESTAURANTS[restaurant]);
+		title.setSpan(new RelativeSizeSpan((float)2.2), 0, title.length(), 0);
+		((TextView) findViewById(R.restaurantDetailsPage.restaurantName)).setText(title); 
 		((TextView) findViewById(R.restaurantDetailsPage.restaurantHours)).setText(WeeklySchedule());
 		
 		((Button) findViewById(R.restaurantDetailsPage.to_map_button)).setOnClickListener(mapListener);
 
 	}
 	
+	// TODO place holder functions below
+	
 	CharSequence WeeklySchedule() {
 		SpannableStringBuilder out = new SpannableStringBuilder();
 		
-		// TODO This is temporary till the database is implemented
-		
-		out.append("S " + Main.SUNDAY_START[restaurant] + " - " + Main.SUNDAY_END[restaurant] + '\n' + 
-		"M " + Main.MONDAY_START[restaurant] + " - " + Main.MONDAY_END[restaurant] + '\n' +
-		"T " + Main.TUESDAY_START[restaurant] + " - " + Main.TUESDAY_END[restaurant] + '\n' + 
-		"W " + Main.WEDNESDAY_START[restaurant] + " - " + Main.WEDNESDAY_END[restaurant] + '\n' + 
-		"T " + Main.THURSDAY_START[restaurant] + " - " + Main.THURSDAY_END[restaurant] + '\n' + 
-		"F " + Main.FRIDAY_START[restaurant] + " - " + Main.FRIDAY_END[restaurant] + '\n' + 
-		"M " + Main.SATURDAY_START[restaurant] + " - " + Main.SATURDAY_END[restaurant] + '\n');
+		out.append("S\t" + Main.SUNDAY_START[restaurant] + " - " + Main.SUNDAY_END[restaurant] + '\n' + 
+		"M\t" + Main.MONDAY_START[restaurant] + " - " + Main.MONDAY_END[restaurant] + '\n' +
+		"T\t" + Main.TUESDAY_START[restaurant] + " - " + Main.TUESDAY_END[restaurant] + '\n' + 
+		"W\t" + Main.WEDNESDAY_START[restaurant] + " - " + Main.WEDNESDAY_END[restaurant] + '\n' + 
+		"T\t" + Main.THURSDAY_START[restaurant] + " - " + Main.THURSDAY_END[restaurant] + '\n' + 
+		"F\t" + Main.FRIDAY_START[restaurant] + " - " + Main.FRIDAY_END[restaurant] + '\n' + 
+		"M\t" + Main.SATURDAY_START[restaurant] + " - " + Main.SATURDAY_END[restaurant] + '\n');
 		
 		int i = 0;
 		for (int day = 1;day<=7;day++) {
@@ -64,7 +68,7 @@ public class RestaurantDetails extends Activity {
 			next = (next != -1 ? next : out.length());
 			
 			if (day==rightNow.get(Calendar.DAY_OF_WEEK))
-				out.setSpan(new RelativeSizeSpan((float)1.4), i, next, 0);  // trying to find way to do bold but had to settle for this
+				out.setSpan(new RelativeSizeSpan((float)1.6), i, next, 0);  // trying to find way to do bold but had to settle for this
 			i = next + 1;
 		}
 		
