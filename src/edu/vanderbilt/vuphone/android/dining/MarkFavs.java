2f88bd3a01a1c914f2b3ba2482ce971b0231fa70
ptr.holly <ptr.holly@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-11-05 04:14:50 +0000

    Added buttons to the dialogs in the MarkFavs activity making them less annoying.

--- src/edu/vanderbilt/vuphone/android/dining/MarkFavs.java
+++ src/edu/vanderbilt/vuphone/android/dining/MarkFavs.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
-import android.view.Menu;
+import android.view.KeyEvent;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,9 +20,12 @@ import android.widget.ListView;
  */
 public class MarkFavs extends ListActivity {
 
-	/**Indicates what dialog is displayed when showDialog is called*/
-	private static final int DIALOG_ITEM_START = 0, DIALOG_ITEM_DISPLAY_CHECKED = 1;
-	/**List of the position in list of the restaurants that are selected*/
+	/** Indicates what dialog is displayed when showDialog is called */
+	private static final int DIALOG_ITEM_START = 0,
+			DIALOG_ITEM_DISPLAY_CHECKED = 1;
+	/** Indicates which dialog the button press is from */
+	private static final int BUTTON_START = -1, BUTTON_DISPLAY_CHECKED = -2;
+	/** List of the position in list of the restaurants that are selected */
 	private ArrayList<Integer> clickedPositions;
 
 	@Override
@@ -28,7 +33,7 @@ public class MarkFavs extends ListActivity {
 		super.onCreate(ice);
 
 		showDialog(DIALOG_ITEM_START);
-		
+
 		Bundle extras = getIntent().getExtras();
 		int[] favorites = extras.getIntArray("favorites");
 
@@ -37,17 +42,11 @@ public class MarkFavs extends ListActivity {
 				Main.RESTAURANTS));
 		getListView().setTextFilterEnabled(true);
 		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-		
-		for( int x = 0; x < favorites.length; ++x){
-			getListView().setItemChecked(favorites[x],true);
+
+		for (int x = 0; x < favorites.length; ++x) {
+			getListView().setItemChecked(favorites[x], true);
 		}
 
-	}
-
-	/** Makes menu button function as a done button */
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getClickedPositions();
-		return true;
 	}
 
 	/** generates an array of the positions that are clicked */
@@ -63,6 +62,21 @@ public class MarkFavs extends ListActivity {
 		showDialog(DIALOG_ITEM_DISPLAY_CHECKED);
 	}
 
+	/** Handles the results of button presses from dialogs */
+	private OnClickListener listener = new OnClickListener() {
+
+		@Override
+		public void onClick(DialogInterface arg0, int arg1) {
+			arg0.dismiss();
+			switch (arg1) {
+			case BUTTON_DISPLAY_CHECKED:
+				finish();
+				break;
+			default:
+			}
+		}
+	};
+
 	/** Creates the dialogs used in the MarkFavs view */
 	protected Dialog onCreateDialog(int id) {
 		AlertDialog dialog;
@@ -71,16 +85,32 @@ public class MarkFavs extends ListActivity {
 
 		case DIALOG_ITEM_DISPLAY_CHECKED:
 			builder.setMessage("the items selected are " + clickedPositions);
+			dialog = builder.create();
+			dialog.setButton(BUTTON_DISPLAY_CHECKED, "OK", listener);
 			break;
 
 		case DIALOG_ITEM_START:
 			builder.setMessage("Press menu key when done making selections");
+			dialog = builder.create();
+			dialog.setButton(BUTTON_START,"OK", listener);
 			break;
 
 		default:
 			return null;
 		}
-		dialog = builder.create();
+		
 		return dialog;
+	}
+
+	/**
+	 * Displays the checked locations and finishes the activity when menu is
+	 * pressed
+	 */
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			getClickedPositions();
+
+		}
+		return false;
 	}
 }
