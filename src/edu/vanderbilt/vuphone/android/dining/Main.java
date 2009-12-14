ab27e287bada899579d85662ea2ef36de174cd7a
ptr.holly <ptr.holly@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-12-14 10:18:01 +0000

    the user can now sort the list also the list gets resorted whenever the main screen reloads to reflect changes made in markfavs

--- src/edu/vanderbilt/vuphone/android/dining/Main.java
+++ src/edu/vanderbilt/vuphone/android/dining/Main.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Random;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,30 +34,48 @@ public class Main extends ListActivity {
 	private static final int MENU_ITEM_VIEW_MAP = 0;
 	/** The second case in the menu */
 	private static final int MENU_ITEM_MARK_FAVS = 1;
-	private static final int MENU_ITEM_OPTIONS = 2;
+	/** The case in menu to change sorting method */
+	private static final int MENU_ITEM_CHOOSE_SORTING = 2;
+	/** The custom adapter used to create the list */
+	private RestaurantAdapter ra;
+	/**
+	 * This value saves the users sort preference while using the program and
+	 * and defaults to showing favorites on top followed by open and then closed
+	 * locations
+	 */
+	private int currentSortMethod = RestaurantAdapter.FAVORITE_OPEN_CLOSED;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 
 		// clunky mechanic with gross file dependency,
-		// but DBWrapper needs Main.applicationContext for its
+		// but DBWrapper needs Main.mainContext for its
 		// calls to DBAdapter
 		if (applicationContext == null)
 			applicationContext = getApplicationContext();
-		
-		//deleteAllRestaurants();
-		//addRandomRestaurantsToDB(20);
-				
-		RestaurantAdapter ra = new RestaurantAdapter(this, RestaurantAdapter.FAVORITE_OPEN_CLOSED);
-		
-		//ra.setSort(RestaurantAdapter.FAVORITE_OPEN_CLOSED);
-		setListAdapter(ra); // redisplay using correct sorting
-		
+
+		// deleteAllRestaurants();
+		// addRandomRestaurantsToDB(20);
+
+		ra = new RestaurantAdapter(this, currentSortMethod);
+		setListAdapter(ra);
 		getListView().setTextFilterEnabled(true);
-		
+	}
+
+	/** Resorts the list when it is resumed */
+	public void onResume() {
+		super.onResume();
+		ra.setSort(currentSortMethod);
+		setListAdapter(ra);
+	}
+
+	/** Sets a new sort method to be used and resorts the list */
+	protected void reSortList(int sortMethod) {
+		currentSortMethod = sortMethod;
+		ra.setSort(currentSortMethod);
+		setListAdapter(ra);
 	}
 
 	protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -70,11 +91,46 @@ public class Main extends ListActivity {
 		startActivity(toDetails);
 	}
 
+	/**This opens the dialog that allows the user to choose a new sorting option*/
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog;
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		final CharSequence[] items = { "open/closed", "near/far",
+				"favorites/open/closed", "favorite", "alphabetical" };
+		final ArrayList<Integer> itemsIDs = new ArrayList<Integer>();
+		itemsIDs.add(RestaurantAdapter.OPEN_CLOSED);
+		itemsIDs.add(RestaurantAdapter.NEAR_FAR);
+		itemsIDs.add(RestaurantAdapter.FAVORITE_OPEN_CLOSED);
+		itemsIDs.add(RestaurantAdapter.FAVORITE);
+		itemsIDs.add(RestaurantAdapter.ALPHABETICAL);
+
+		builder.setSingleChoiceItems(items,
+				itemsIDs.indexOf(currentSortMethod),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int item) {
+
+						reSortList(itemsIDs.get(item));
+
+					}
+				});
+
+		builder.setNeutralButton("Done", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int item) {
+				dialog.cancel();
+			}
+		});
+		builder.setTitle("Choose sort method");
+		dialog = builder.create();
+
+		return dialog;
+	}
+
 	/** Creates list of actions for user when the menu button is clicked */
-	public boolean onCreateOptionsMenu(Menu menu) { // do we need to call the super here?
-		menu.add(Menu.NONE, MENU_ITEM_VIEW_MAP, Menu.NONE, "View Map");
-		menu.add(Menu.NONE, MENU_ITEM_OPTIONS, Menu.NONE, "Sorting");
-		menu.add(Menu.NONE, MENU_ITEM_MARK_FAVS, Menu.NONE, "Mark Favorites");
+	public boolean onCreateOptionsMenu(Menu menu) { // do we need to call the
+		// super here?
+		menu.add(0, MENU_ITEM_VIEW_MAP, 0, "View Map");
+		menu.add(0, MENU_ITEM_MARK_FAVS, 0, "Mark Favorites");
+		menu.add(0, MENU_ITEM_CHOOSE_SORTING, 0, "Sort");
 		return true;
 	}
 
@@ -83,20 +139,21 @@ public class Main extends ListActivity {
 		super.onOptionsItemSelected(item);
 		switch (item.getItemId()) {
 		case MENU_ITEM_VIEW_MAP:
-			Intent toMap = new Intent(this, AllLocations.class);
-//			i.putExtra(AllLocations.EXTRA_LONGITUDES, LOCATION_LONGITUDES);
-//			i.putExtra(AllLocations.EXTRA_LATITUDES, LOCATION_LATITUDES);
-//			i.putExtra(AllLocations.EXTRA_LOCATIONS, RESTAURANTS);
-			startActivity(toMap);
+			Intent i = new Intent(this, AllLocations.class);
+			startActivity(i);
 			return true;
 		case MENU_ITEM_MARK_FAVS:
-			Intent toMarkFavs = new Intent(this, MarkFavs.class);
-//			i2.putExtra(MarkFavs.EXTRA_FAVORITES, FAVORITES);
-			startActivity(toMarkFavs);
+			Intent i2 = new Intent(this, MarkFavs.class);
+			long[] order = new long[ra.getSortOrder().size()];
+			for (int x = 0; x < ra.getSortOrder().size(); ++x) {
+				order[x] = ra.getSortOrder().get(x);
+			}
+			i2.putExtra(MarkFavs.ADAPTER, order);
+			startActivity(i2);
 			return true;
-		case MENU_ITEM_OPTIONS:
-			Intent toOptions = new Intent(this, Options.class);
-			startActivity(toOptions);
+		case MENU_ITEM_CHOOSE_SORTING:
+			showDialog(1);
+			return true;
 		default:
 			return false;
 		}
